9d3112f60243df58709b28f65bbd722dacb10790
Irving Juárez <irving.juarez@thomsonreuters.com>
2023-10-09 17:49:03 -0600

    Connection to postgres db created

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const routing = require('./routers');
+const client = require('./libs/postgres');
 
 const app = express();
-routing(app);
+
+function onSuccess() {
+    console.log('Successfully connected to the database!');
+    routing(app);
+}
+
+function onFail(reason) {
+    console.log('Failed to connect to the database');
+    console.log(reason);
+}
+
+client.connect()
+    .then(onSuccess, onFail)
---
+++ libs/postgres.js
@@ -0,0 +1,13 @@
+const { Client } = require('pg');
+
+const config = {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
+}
+
+const client = new Client(config)
+
+module.exports = client;
