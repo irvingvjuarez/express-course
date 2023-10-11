76e2555ff60b0cf909abc2f3125b68b32a0ca7af
Irving Juárez <irving.juarez@thomsonreuters.com>
2023-10-10 18:00:50 -0600

    Client and Pool connection to the database done

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,12 +1,5 @@
 const { Client } = require('pg');
-
-const config = {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB
-}
+const config = require('../config/db.config');
 
 const client = new Client(config)
 
